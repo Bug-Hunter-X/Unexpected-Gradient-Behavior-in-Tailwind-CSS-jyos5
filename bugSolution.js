a81cfbb0a26b8cd8d22466fc1cc289a3347ff42e
BugHunter-X <askzenai@gmail.com>
2025-01-27 02:02:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+// in your component file
+<div className="relative bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-md">
+  <div className="absolute inset-0 bg-white opacity-20"></div>
+  <h1>Hello, Tailwind!</h1>
+</div>
+```
